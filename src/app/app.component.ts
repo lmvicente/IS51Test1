b323a8bf41f220d058dbba5f8e090d4230da9b45
lmvicente <leah.m.vicente@gmail.com>
2018-10-18 17:33:45 -0700

    updated buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FlexModalService } from './shared-components/flex-modal/flex-modal.service';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,13 +11,17 @@ import { FlexModalService } from './shared-components/flex-modal/flex-modal.serv
 export class AppComponent implements OnInit {
   title = 'final';
   aboutText: string;
-  constructor(private flexModal: FlexModalService) { 
+
+  constructor(private flexModal: FlexModalService) {
 
   }
 
   ngOnInit() {
+    this.aboutText = "This application is designed by Leah Vicente. (c) 2018";
   }
 
   // Launch flex modal or alert (3pts reduced) showing about text
-
+  aboutModal() {
+    this.flexModal.openDialog('about-modal');
+  }
 }
--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -3,6 +3,16 @@ import { Router } from '@angular/router';
 import { FlexModalService } from '../shared-components/flex-modal/flex-modal.service';
 import { Http } from '@angular/http';
 
+// Variables declared
+// use ? for variables
+export interface IOrder {
+  pid?: string;
+  image?: string;
+  description?: string;
+  price?: number;
+  quantity?: number;
+}
+
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
@@ -11,7 +21,7 @@ import { Http } from '@angular/http';
 
 export class OrdersComponent implements OnInit {
 
-  orders: Array<any> = [];
+  orders: Array<IOrder> = [];
 
   constructor(
     private router: Router,
@@ -24,4 +34,86 @@ export class OrdersComponent implements OnInit {
 
   }
 
+  // Add Orders
+  addItem(item: string) {
+    switch (item) {
+      case 'Android':
+        this.orders.unshift({
+          'pid': '1',
+          'image': 'assets/sm_android.jpeg',
+          'description': 'Android',
+          'price': 150.00,
+          'quantity': 1
+        });
+        break;
+      case 'IPhone':
+        this.orders.unshift({
+          'pid': '2',
+          'image': 'assets/sm_iphone.jpeg',
+          'description': 'IPhone',
+          'price': 200.00,
+          'quantity': 1
+        });
+        break;
+      case 'Windows':
+        this.orders.unshift({
+          'pid': '3',
+          'image': 'assets/sm_windows.jpeg',
+          'description': 'Windows Phone',
+          'price': 110.00,
+          'quantity': 1
+        });
+        break;
+    }
+  }
+  // Display Orders
+  displayOrder() {
+    this.orders = [{
+      'pid': '1',
+      'image': 'assets/sm_android.jpeg',
+      'description': 'Android',
+      'price': 150.00,
+      'quantity': 2
+    }, {
+      'pid': '2',
+      'image': 'assets/sm_iphone.jpeg',
+      'description': 'IPhone',
+      'price': 200.00,
+      'quantity': 1
+    }, {
+      'pid': '3',
+      'image': 'assets/sm_windows.jpeg',
+      'description': 'Windows Phone',
+      'price': 110.00,
+      'quantity': 2
+    }];
+
+  }
+
+  // Clear Orders
+  clearOrders() {
+    this.orders.map((item: IOrder, i: number) => {
+      Object.keys(item).map((key: string) => {
+        if (key !== 'image') {
+          item[key] = '';
+        }
+        return item;
+      });
+    });
+  }
+
+  // Submit button “Thank you . Here is your order details!”.
+  submitOrder() {
+
+  }
+  // Validation button
+  validateOrder() {
+
+  }
+
+  deleteOrder(index: number) {
+    console.log(index);
+    this.orders.splice(index, 1);
+  }
 }
+
